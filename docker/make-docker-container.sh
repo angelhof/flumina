10280a065783c208d874c8cae374bd16c4b0c9bb
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-04-02 10:50:53 -0400

    Docker script that runs a container connected to a docker network named 'temp'

--- docker/make-docker-container.sh
+++ docker/make-docker-container.sh
@@ -1,3 +1,8 @@
+## Note:
+## At the moment all the nodes have the same read
+## only directory
+##
+## TODO: Maybe they should pull from github?
 mkdir -p ./read-only
 cp -r ../erlang-dot read-only/erlang-dot
 cp -r ../examples read-only/examples
@@ -8,5 +13,15 @@ cp ../Makefile read-only/Makefile
 
 rm -f read-only/ebin/*
 
+set -x
+
+## Notes:
+## We need to use fully qualified names for hostname, so names with a dot, e.g. node1.local
+##
+## Call this script with a name for the node as a first argument
 docker run -e ERL_TOP='/usr/local/'\
-       -v "${PWD}/read-only/":/stream-processing-prototype -it --rm -h erlang.local erlang:21.0 bash
+       -v "${PWD}/read-only/":/stream-processing-prototype -it --rm\
+       --network temp\
+       --name "$1.local"\
+       --hostname "$1.local"\
+       erlang:21.0 erl -name "$1@$1.local" -setcookie docker
