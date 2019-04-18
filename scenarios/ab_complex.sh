320aeb06eb67d050f85d6b54c523c52840fc7085
Filip Niksic <fniksic@seas.upenn.edu>
2019-04-17 20:26:12 -0400

    Change ab_ scenarios

--- scenarios/ab_complex.sh
+++ scenarios/ab_complex.sh
@@ -3,17 +3,25 @@
 A1="a1node"
 A2="a2node"
 A3="a3node"
+A4="a4node"
+A5="a5node"
 B="main"
 
-RATE_MULTI=40
+RATE_MULTI=20
 RATIO_AB=1000
-HEARTBEAT_RATIO=100
-EXEC="-noshell -run util exec abexample. distributed_experiment. [['${B}@${B}.local','${A1}@${A1}.local','${A2}@${A2}.local','${A3}@${A3}.local'],${RATE_MULTI},${RATIO_AB},${HEARTBEAT_RATIO}]. -s erlang halt"
+HEARTBEAT_RATIO=10
+optimizer="optimizer_greedy"
+
+erlArgs="-noshell -run util exec abexample. distributed_experiment. [['${B}@${B}.local','${A1}@${A1}.local','${A2}@${A2}.local','${A3}@${A3}.local','${A4}@${A4}.local','${A5}@${A5}.local'],${RATE_MULTI},${RATIO_AB},${HEARTBEAT_RATIO},${optimizer}]. -s erlang halt"
 
 simulTime=60 # seconds
+delay="6560ns"
+dataRate="100Mbps"
+
+ns3Args="--TotalTime=${simulTime} --ns3::CsmaChannel::DataRate=${dataRate} --ns3::CsmaChannel::Delay=${delay}"
 
 ./ns3/simulate.sh -m "${B}" \
-  -e "${EXEC}" \
-  -n "--TotalTime=${simulTime}" \
-  ${A1} ${A2} ${A3}
+  -e "${erlArgs}" \
+  -n "${ns3Args}" \
+  ${A1} ${A2} ${A3} ${A4} ${A5}
 
