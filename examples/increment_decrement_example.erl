8f52ec0ae8f1782871d5ebaeaf0be513877344ec
Caleb Stanford <caleb.pirsquared@gmail.com>
2019-04-23 23:16:30 -0400

    incdec

--- examples/increment_decrement_example.erl
+++ examples/increment_decrement_example.erl
@@ -6,53 +6,52 @@
 -include_lib("eunit/include/eunit.hrl").
 -include("type_definitions.hrl").
 
-% %% =====================================
-% %% ===== Computation Specification =====
-% %% =====================================
-%
-% %% State types, events, and dependencies
-% -type state() :: {integer(), integer()}. % Total today and max today
-%
-% -type event_i() :: message({'i',integer()},'none').
-% -type event_d() :: message({'d',integer()},'none').
-% -type event_sync() :: message('#', 'none').
-% -type events() :: event_i() | event_d() | event_sync().
-%
-% -type event_tags() :: {'i',integer()} | {'d',integer()} | '#'.
-%
-% dependencies() ->
-%     #{'i' => ['d','#'],
-%       'd' => ['i','#'],
-%       '#' => ['i','d','#']
-%      }.
-%
-% %% Sequential Specification
-% -spec init_state() -> state().
-% init_state() -> {0,0}.
-%
-% -spec update(events(), state(), pid()) -> state().
-% update({'#', 'none'}, {_Total, Max}, SinkPid) ->
-%     SinkPid ! {Max},
-%     {0,0};
-% update({{Tag,_ImpID}, 'none'}, {Total, Max}, _SinkPid) ->
-%     case Tag of
-%         'i' ->
-%             NewTotal = Total + 1;
-%         'd' ->
-%             NewTotal = Total - 1
-%     end,
-%     NewMax = max(NewTotal,Max),
-%     {NewTotal,NewMax}.
-%
-% %% Parallelization Primitives
-% -spec fork(split_preds(), state()) -> {state(), state()}.
-% fork(_, {Total, Max}) ->
-%     {{Total, Max}, {0, 0}}.
-%
-% -spec join(state(), state()) -> state().
-% join({X1, Y1}, {X2, _Y2}) -> % Y2 not used
-%     X = X1 + X2,
-%     {X, max(Y1,X)}.
+%% =====================================
+%% ===== Computation Specification =====
+%% =====================================
+
+%% State types, events, and dependencies
+-type state() :: {integer(), integer()}. % Total today and max today
+-type events() :: message('i', 'none')
+                | message('d', 'none')
+                | message('#', 'none').
+-type event_tags() :: 'i' | 'd' | '#'.
+
+-spec dependencies() -> #{event_tags() := [event_tags()]}.
+dependencies() ->
+    #{'i' => ['d','#'],
+      'd' => ['i','#'],
+      '#' => ['i','d','#']
+     }.
+
+%% Sequential Specification
+-spec init_state() -> state().
+init_state() -> {0,0}.
+
+-spec update(events(), state(), pid()) -> state().
+update({'#', 'none'}, {_Total, Max}, SinkPid) ->
+    SinkPid ! {Max},
+    {0,0};
+update({Tag, 'none'}, {Total, Max}, _SinkPid) ->
+    case Tag of
+        'i' ->
+            NewTotal = Total + 1;
+        'd' ->
+            NewTotal = Total - 1
+    end,
+    NewMax = max(NewTotal,Max),
+    {NewTotal,NewMax}.
+
+%% Parallelization Primitives
+-spec fork(split_preds(), state()) -> {state(), state()}.
+fork(_, {Total, Max}) ->
+    {{Total, Max}, {0, 0}}.
+
+-spec join(state(), state()) -> state().
+join({X1, Y1}, {X2, _Y2}) -> % Y2 not used
+    X = X1 + X2,
+    {X, max(Y1,X)}.
+
 %
 % %% ============================
 % %% ===== Experiment Setup =====
