fd0e85019031bd1597ea589e7f51fa5c4976def0
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-10-30 19:24:32 -0400

    Add timestamps in kddcup file

--- data/prepare_outlier_detection.sh
+++ data/prepare_outlier_detection.sh
@@ -3,3 +3,4 @@
 # Downloads and extracts the dataset for outlier detection
 wget http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data.gz
 gunzip kddcup.data.gz
+python3 kddcup_add_timestamps.py kddcup.data kddcup_timestamps.data
