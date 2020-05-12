271ef71175b356ead0b40f3c778b9367e200a958
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-05-12 22:08:22 +0000

    Script that starts erlang nodes, performs basic experiment and stops nodes

--- experiments/scripts/start_erlang_nodes.sh
+++ experiments/scripts/start_erlang_nodes.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-ssh -i ~/.ssh/internal_rsa ubuntu@ec2-18-188-245-57.us-east-2.compute.amazonaws.com erl -detached -noinput -sname flumina1 -setcookie flumina 
+echo "Starting the nodes"
+ssh -i ~/.ssh/internal_rsa ubuntu@ip-172-31-41-102.us-east-2.compute.internal erl -detached -noinput -sname flumina1 -setcookie flumina -pa flumina-devel/ebin flumina-devel/experiments/ebin
+
+ssh -i ~/.ssh/internal_rsa ubuntu@ip-172-31-38-231.us-east-2.compute.internal erl -detached -noinput -sname flumina2 -setcookie flumina -pa flumina-devel/ebin flumina-devel/experiments/ebin
+
+echo "Running the main abexample experiment"
+make exec NAME_OPT="-sname main -setcookie flumina" args="abexample. real_distributed. [[\'flumina1@ip-172-31-41-102\',\'flumina2@ip-172-31-38-231\',\'main@ip-172-31-35-213\']]."
+
+echo "Closing the nodes"
+echo "rpc:call('flumina1@ip-172-31-41-102', init, stop, [])." | erl -sname main -setcookie flumina
+echo "rpc:call('flumina2@ip-172-31-38-231', init, stop, [])." | erl -sname main -setcookie flumina
+
