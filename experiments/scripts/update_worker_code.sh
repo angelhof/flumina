11742d3623e39b4d6b9ec65831e018dfa3e91673
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-05-24 15:48:00 +0000

    Extend update script to not update binaries if a host is down

--- experiments/scripts/update_worker_code.sh
+++ experiments/scripts/update_worker_code.sh
@@ -4,7 +4,11 @@ hostname=$1
 
 flumina_dir=/home/ubuntu/flumina-devel
 
-echo "Updating binaries in: $hostname..."
-
-scp -i ~/.ssh/internal_rsa -r -q ${flumina_dir}/ebin ubuntu@$hostname:${flumina_dir}
-scp -i ~/.ssh/internal_rsa -r -q ${flumina_dir}/experiments/ebin ubuntu@$hostname:${flumina_dir}/experiments
+if nc -z -w1 $hostname 22 2>/dev/null ; then
+    echo "Updating binaries in: $hostname..."
+    scp -i ~/.ssh/internal_rsa -r -q ${flumina_dir}/ebin ubuntu@$hostname:${flumina_dir} &&
+        scp -i ~/.ssh/internal_rsa -r -q ${flumina_dir}/experiments/ebin ubuntu@$hostname:${flumina_dir}/experiments &&
+        echo $hostname >> /tmp/updated_hosts
+else
+    echo "Didn't update binaries in $hostname because it is not accessible..."
+fi
--- experiments/scripts/update_workers_code.sh
+++ experiments/scripts/update_workers_code.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+rm /tmp/updated_hosts
+
 cat ec2/hostnames |
     xargs -L 1 scripts/update_worker_code.sh
 
-echo "Updated $(wc -l ec2/hostnames | cut -f 1 -d ' ') workers."
+echo "Updated $(wc -l /tmp/updated_hosts | cut -f 1 -d ' ') workers."
