989d8cd3cfddc7ed6424bb2309863b4b380079df
Filip Niksic <fniksic@seas.upenn.edu>
2020-05-25 13:03:35 -0400

    Fix a bug in flink_run.sh

--- experiments/scripts/flink_run.sh
+++ experiments/scripts/flink_run.sh
@@ -16,16 +16,16 @@ cp flink_workers ${FLINK_HOME}/conf/slaves
 
 # Start the cluster -- we want to start it from the home
 # directory
-PWD="$(pwd)"
+DIR="${PWD}"
 cd ${HOME}
 ${FLINK_HOME}/bin/start-cluster.sh
 
 # Start the experiment
-cd ${PWD}
-python experiment/ \
+cd ${DIR}
+python ./experiment/ \
   -s ${SUITE} \
   --flink-workers flink_workers \
-  >/dev/null 2>&1 &
+  >./exp.log 2>&1 &
 
 echo -e "\nThe experiment is now running. You can detach the process by running"
 echo -e "\n  disown -ah\n"
