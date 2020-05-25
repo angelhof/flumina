817d7bf02b9df962877d70b889e0e7f122a9f5e8
Filip Niksic <fniksic@seas.upenn.edu>
2020-05-25 12:46:15 -0400

    Add a few scripts for running Flink jobs

---
+++ experiments/scripts/flink_logs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# The input parameters:
+#   - experiment suite
+SUITE="${1}"
+
+# Create the logs directory
+mkdir ./archive/${SUITE}/logs
+
+# Make sure the Flink cluster is stopped
+${FLINK_HOME}/bin/stop-cluster.sh
+
+# Collect and clean the worker logs
+for host in $(cat flink_workers); do
+  mkdir ./archive/${SUITE}/logs/${host}
+  scp ${host}:flink/log/* ./archive/${SUITE}/logs/${host}/
+  ssh ${host} "rm ./flink/log/*"
+done
+
+mkdir ./archive/${SUITE}/logs/jobmanager
+mv ${FLINK_HOME}/log/* ./archive/${SUITE}/logs/jobmanager
---
+++ experiments/scripts/flink_run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# The input parameters are:
+#   - the experiment suite to run
+#     (check experiment/suites.py or run python experiment/ -l)
+#   - the number of Flink workers
+
+SUITE="${1}"
+COUNT="${2}"
+# Make sure the Python virtual environment is active
+source .venv/bin/activate
+
+# Generate Flink workers
+./scripts/flink_workers.sh ${COUNT} > flink_workers
+cp flink_workers ${FLINK_HOME}/conf/slaves
+
+# Start the cluster -- we want to start it from the home
+# directory
+PWD="$(pwd)"
+cd ${HOME}
+${FLINK_HOME}/bin/start-cluster.sh
+
+# Start the experiment
+cd ${PWD}
+python experiment/ \
+  -s ${SUITE} \
+  --flink-workers flink_workers \
+  >/dev/null 2>&1 &
+
+echo "The experiment is now running. You can detach the process by running"
+echo -e "\n  disown -ah\n"
+echo "Once the process is detached, you can log out."
