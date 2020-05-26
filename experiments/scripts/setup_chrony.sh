775c93224efc0a119527e89e12a7543ce3e6d65b
Filip Niksic <fniksic@seas.upenn.edu>
2020-05-25 21:53:28 -0400

    Fix the setup_chrony.sh script

--- experiments/scripts/setup_chrony.sh
+++ experiments/scripts/setup_chrony.sh
@@ -7,6 +7,4 @@ ssh ${HOST} << EOF
   sudo apt-get install -y chrony
   sudo sed -i "4i# Amazon Time Sync Service\nserver 169.254.169.123 prefer iburst minpoll 4 maxpoll 4\n" /etc/chrony/chrony.conf
   sudo /etc/init.d/chrony restart
-  chronyc sources -v
-  chronyc tracking
 EOF
