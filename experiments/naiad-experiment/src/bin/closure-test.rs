a7fec8a5809b76989209aa743a136b31cdc959aa
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-09-03 16:12:18 -0400

    remove immutable vars from example

--- experiments/naiad-experiment/src/bin/closure-test.rs
+++ experiments/naiad-experiment/src/bin/closure-test.rs
@@ -1,24 +1,17 @@
 use std::vec::Vec;
 
 fn main() {
-    let val = 3;
-    let vec = vec![0; 5];
+    let mut val = 3;
+    let mut vec = Vec::new();
 
-    let mut val_mut = 3;
-    let mut vec_mut = Vec::new();
-
-    vec_mut.push(0);
+    vec.push(0);
 
     let mut do_something = move |x: i64| {
-        // val += x; // error: val is immutable
-        val_mut += x;
-        // vec.push(x); // error: vec is immutable
-        vec_mut.push(x);
+        val += x;
+        vec.push(x);
         println!("input: {}", x);
         println!("  val: {}", val);
-        println!("  val_mut: {}", val_mut);
         println!("  vec: {:?}", vec);
-        println!("  vec_mut: {:?}", vec_mut);
     };
 
     do_something(3);
@@ -27,8 +20,8 @@ fn main() {
     // Doesn't work (whether or not the closure uses move)
     // Problem is that the closure takes ownership of mutable variables
     // (or borrows mutably), preventing further mutation
-    // val_mut += 3;
-    // vec_mut.push(3);
+    // val += 3;
+    // vec.push(3);
 
     do_something(3);
     do_something(1);
