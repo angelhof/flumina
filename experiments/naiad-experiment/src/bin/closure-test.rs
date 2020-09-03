333382eb24fdc9ef78fe64150479803136225938
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-09-03 16:10:35 -0400

    Rust closure test -- comment code that doesn't work

--- experiments/naiad-experiment/src/bin/closure-test.rs
+++ experiments/naiad-experiment/src/bin/closure-test.rs
@@ -1,14 +1,15 @@
-
 use std::vec::Vec;
 
 fn main() {
     let val = 3;
-    let mut val_mut = 3;
     let vec = vec![0; 5];
+
+    let mut val_mut = 3;
     let mut vec_mut = Vec::new();
+
     vec_mut.push(0);
 
-    let mut do_something = |x: i64| {
+    let mut do_something = move |x: i64| {
         // val += x; // error: val is immutable
         val_mut += x;
         // vec.push(x); // error: vec is immutable
@@ -22,6 +23,13 @@ fn main() {
 
     do_something(3);
     do_something(2);
+
+    // Doesn't work (whether or not the closure uses move)
+    // Problem is that the closure takes ownership of mutable variables
+    // (or borrows mutably), preventing further mutation
+    // val_mut += 3;
+    // vec_mut.push(3);
+
     do_something(3);
     do_something(1);
 
