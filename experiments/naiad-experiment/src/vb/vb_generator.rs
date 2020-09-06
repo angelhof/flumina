9ba2d8753430cdf8a2f259b525dc331464ce53e0
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-09-05 20:14:00 -0400

    make value generator parametric in frequency and total time

--- experiments/naiad-experiment/src/vb/vb_generator.rs
+++ experiments/naiad-experiment/src/vb/vb_generator.rs
@@ -13,15 +13,19 @@ use timely::dataflow::operators::generic::operator::source;
 use timely::dataflow::scopes::Scope;
 use timely::dataflow::stream::Stream;
 
+use std::io;
+use std::fmt::Debug;
+use std::str::FromStr;
 use std::time::{Duration, SystemTime};
 
 mod vb_data;
 use vb_data::{VBData, VBItem};
 
 /*
-    Utility functions related to time
+    Utility functions
 */
 
+// Related to time
 fn time_since(t: SystemTime) -> Duration {
     // Note: this function may panic in case of clock drift
     return t.elapsed().unwrap();
@@ -30,13 +34,32 @@ fn div_durations(d1: Duration, d2: Duration) -> u64 {
     ((d1.as_nanos() as f64) / (d2.as_nanos() as f64)) as u64
 }
 
+// For stdin input
+fn get_input<T>(msg: &str) -> T
+where
+    T: FromStr,
+    <T as FromStr>::Err: Debug,
+{
+    println!("{}", msg);
+    let mut input_text = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("failed to read from stdin");
+    input_text.trim().parse::<T>().expect("not an integer")
+}
+
 /*
     The main value source
 */
 
 type Item = VBItem<u128>;
 
-fn value_source<G>(scope: &G, loc: usize) -> Stream<G, Item>
+fn value_source<G>(
+    scope: &G,
+    loc: usize,
+    frequency: Duration,
+    total: Duration,
+) -> Stream<G, Item>
 where
     G: Scope<Timestamp = u64>,
 {
@@ -49,14 +72,10 @@ where
         let activator = scope.activator_for(&info.address[..]);
         let mut maybe_cap = Some(capability);
 
-        // Parameters
-        let frequency = Duration::from_micros(10);
-        let total = Duration::from_secs(10);
-        let vals_max = div_durations(total, frequency);
-
-        // Initialize with current time
+        // Initialize with current time; keep track of # vals sent
         let start_time = SystemTime::now();
         let mut vals_sent = 0;
+        let vals_max = div_durations(total, frequency);
 
         // Return closure
         move |output: &mut OutputHandle<u64, Item, Tee<u64, Item>>| {
@@ -97,10 +116,14 @@ where
 */
 
 fn main() {
-    timely::execute_from_args(std::env::args(), |worker| {
+
+    let frequency = Duration::from_micros(get_input("Frequency in microseconds:"));
+    let total = Duration::from_secs(get_input("Total time to run in seconds:"));
+
+    timely::execute_from_args(std::env::args(), move |worker| {
         let w_index = worker.index();
         worker.dataflow(|scope| {
-            value_source(scope, w_index)
+            value_source(scope, w_index, frequency, total)
             .inspect(|x| println!("item generated: {:?}", x));
         });
     }).unwrap();
