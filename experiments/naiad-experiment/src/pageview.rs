94ec9f3cbc96d68d9a9d02fd9d60b0c75a956685
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-09-23 21:28:06 -0400

    refactor pageview experiment params into ExperimentData struct

--- experiments/naiad-experiment/src/pageview.rs
+++ experiments/naiad-experiment/src/pageview.rs
@@ -16,7 +16,40 @@ use timely::dataflow::operators::{Inspect};
 // use timely::dataflow::operators::{Accumulate, Broadcast, Exchange, Filter,
 //                                   Inspect, Map, Reclock};
 
+use std::vec::Vec;
 use std::string::String;
+
+/* Experiment data */
+
+#[derive(Debug, Copy, Clone)]
+pub struct PVExperimentData {
+    parallelism: u64,
+    page0_per_page1: u64,
+    views_per_update: u64,
+    events_per_milli: u64,
+    exp_duration_secs: u64,
+}
+impl PVExperimentData {
+    pub fn to_csv(&self) -> String {
+        format!(
+            "{} wkrs, {} page0/page1, {} views/update, {} events/ms, {} s",
+            self.parallelism,
+            self.page0_per_page1,
+            self.views_per_update,
+            self.events_per_milli,
+            self.exp_duration_secs,
+        )
+    }
+    pub fn timely_args(&self) -> Vec<String>
+    {
+        let mut vec : Vec<String> = Vec::new();
+        vec.push("-w".to_string());
+        vec.push(self.parallelism.to_string());
+        vec
+    }
+}
+
+/* Core computation */
 
 fn pv_gen_only<G>(
     pv_stream: &Stream<G, PVItem>,
@@ -29,10 +62,7 @@ where
 }
 
 fn pv_experiment_core<G, O, F>(
-    page0_per_page1: u64,
-    views_per_update: u64,
-    events_per_milli: u64,
-    exp_duration_secs: u64,
+    params: PVExperimentData,
     scope: &G,
     computation: F,
     output_filename: &'static str,
@@ -44,10 +74,10 @@ where
 {
     /* 1. Initialize */
 
-    let page_0_prob = 1.0 / (page0_per_page1 as f64 + 1.0);
-    let update_prob = 1.0 / (views_per_update as f64 + 1.0);
-    let frequency = Duration::from_nanos(1000000 / events_per_milli);
-    let exp_duration = Duration::from_secs(exp_duration_secs);
+    let page_0_prob = 1.0 / (params.page0_per_page1 as f64 + 1.0);
+    let update_prob = 1.0 / (params.views_per_update as f64 + 1.0);
+    let frequency = Duration::from_nanos(1000000 / params.events_per_milli);
+    let exp_duration = Duration::from_secs(params.exp_duration_secs);
 
     /* 2. Create the Dataflow */
 
@@ -67,49 +97,24 @@ where
         &latency_throughput,
         &output_filename,
         move |(latency, throughput)| { format!(
-            "{} page0/page1, {} views/update, {} events/ms, {} s, {} ms, {} events/ms",
-            page0_per_page1,
-            views_per_update,
-            events_per_milli,
-            exp_duration_secs,
-            latency,
-            throughput,
+            "{}, {} ms, {} events/ms",
+            params.to_csv(), latency, throughput
         )}
     );
 }
 
+/* Exposed experiments */
+
 pub fn pv_experiment_gen_only(
-    parallelism: u64,
-    page0_per_page1: u64,
-    views_per_update: u64,
-    events_per_milli: u64,
-    exp_duration_secs: u64,
+    params: PVExperimentData,
     output_filename: &'static str,
 ) {
-    println!(
-        "PVgen Experiment Parameters: \
-         {} wkrs, {} page0/page1, {} views/update, {} events/ms, {} s",
-        parallelism,
-        page0_per_page1,
-        views_per_update,
-        events_per_milli,
-        exp_duration_secs,
-    );
-    // Vector args to pass to timely
-    let mut timely_args : Vec<String> = Vec::new();
-    timely_args.push("-w".to_string());
-    timely_args.push(parallelism.to_string());
-    let args_iter = timely_args.drain(0..);
-    // Execute the dataflow
-    timely::execute_from_args(args_iter, move |worker| {
+    println!("PVgen Experiment Parameters: {}", params.to_csv());
+    timely::execute_from_args(params.timely_args().drain(0..), move |worker| {
         let worker_index = worker.index();
         worker.dataflow(move |scope| {
             pv_experiment_core(
-                page0_per_page1,
-                views_per_update,
-                events_per_milli,
-                exp_duration_secs,
-                scope,
+                params, scope,
                 |s| pv_gen_only(s),
                 output_filename,
             );
