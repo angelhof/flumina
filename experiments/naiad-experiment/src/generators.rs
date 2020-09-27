67101ed2ce3fad6cb08d8a76b1c3d379ec4440f7
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-09-27 15:50:08 -0400

    throttle output in generator for better performance

--- experiments/naiad-experiment/src/generators.rs
+++ experiments/naiad-experiment/src/generators.rs
@@ -7,11 +7,25 @@ use super::util::{div_durations, nanos_timestamp, time_since};
 
 use timely::dataflow::operators::generic::operator::source;
 
+use std::cmp;
+
 /*
     Data source which produces a number of output items
     over time given by an arbitrary function giving the cumulative
     total to produce, and stopping after the uptime is complete.
+
+    For performance reasons, caps outputs at a given point in time
+    by MAX_OUTPUT. See this note from
+    https://github.com/TimelyDataflow/timely-dataflow
+    > At the moment, the implementations of unary and binary operators
+    > allow their closures to send un-bounded amounts of output. This
+    > can cause unwelcome resource exhaustion, and poor performance
+    > generally if the runtime needs to allocate lots of new memory to
+    > buffer data sent in bulk without being given a chance to digest
+    > it. It is commonly the case that when large amounts of data are
+    > produced, they are eventually reduced given the opportunity.
 */
+const MAX_OUTPUT: u128 = 1000;
 fn variable_rate_source<D, F, G, H>(
     mut item_gen: F,
     scope: &G,
@@ -42,7 +56,15 @@ where
             if let Some(cap) = maybe_cap.as_mut() {
                 // Decide how behind we are on outputting values
                 let elapsed = time_since(start_time);
-                let vals_to_send = cumulative_total_fun(elapsed);
+                let vals_to_send = cmp::min(
+                    cumulative_total_fun(elapsed),
+                    vals_sent + MAX_OUTPUT,
+                );
+
+                // For debugging (this is nice because it shows if the system is
+                // under full load -- in this case # of new values will usually
+                // be throttled at MAX_OUTPUT)
+                // println!("New items to send: {}", vals_to_send - vals_sent);
 
                 // Output values to catch up
                 while vals_sent < vals_to_send && vals_sent < vals_max {
