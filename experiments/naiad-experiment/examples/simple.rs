44c124aa1181f450dc263428f9c378756b79362c
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-09-30 17:42:36 -0400

    print init/complete in simple.rs

--- experiments/naiad-experiment/examples/simple.rs
+++ experiments/naiad-experiment/examples/simple.rs
@@ -6,11 +6,13 @@ fn main() {
     timely::execute_from_args(std::env::args(), |worker| {
         let w_index = worker.index();
         let w_total = worker.peers();
+        println!("worker {}/{} start", w_index, w_total);
         worker.dataflow::<(), _, _>(|scope| {
             (0..10).to_stream(scope).inspect(move |x| {
                 println!("worker {}/{} seen: {:?}", w_index, w_total, x)
             });
-        })
+        });
+        println!("worker {}/{} complete", w_index, w_total);
     })
     .unwrap();
 }
