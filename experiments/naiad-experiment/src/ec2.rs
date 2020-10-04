338bb63875500645747f95ee96ebcdac7cd2610b
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-10-04 16:51:31 -0400

    use different port number for successive ec2 experiments

--- experiments/naiad-experiment/src/ec2.rs
+++ experiments/naiad-experiment/src/ec2.rs
@@ -2,21 +2,17 @@
     Configuration for running Timely experiments on EC2.
 */
 
-use super::util::match_line_in_file;
+use super::util::{match_line_in_file, replace_lines_in_file};
 
 use std::process::Command;
 use std::str;
 use std::string::String;
 
-const EC2_HOST_FILE: &str = "hosts/ec2_hosts.txt";
+const EC2_IP_FILE: &str = "hosts/ec2_local_ips.txt";
+const EC2_HOST_TEMP_FILE: &str = "results/temp_ec2_hosts.txt";
 const EC2_REGION: &str = "us-east-2";
-const EC2_PORT: u32 = 4000;
 
-pub fn get_ec2_host_file() -> &'static str {
-    EC2_HOST_FILE
-}
-
-fn get_ec2_ipv4() -> String {
+pub fn get_ec2_ipv4() -> String {
     // example output: 172.31.27.62
     let output = Command::new("curl")
         .arg("http://169.254.169.254/latest/meta-data/local-ipv4")
@@ -34,19 +30,26 @@ fn get_ec2_ipv4() -> String {
     String::from_utf8_lossy(&output.stdout).to_string()
 }
 
-pub fn get_ec2_host_port_str() -> String {
+pub fn get_ec2_host_port_str(ipv4: &str, port: u64) -> String {
     // example output: ip-172-31-27-62.us-east-2.compute.internal:4000
-    let ipv4 = str::replace(&get_ec2_ipv4(), ".", "-");
-    format!("ip-{}.{}.compute.internal:{}", ipv4, EC2_REGION, EC2_PORT)
+    let ipv4 = str::replace(ipv4, ".", "-");
+    format!("ip-{}.{}.compute.internal:{}", ipv4, EC2_REGION, port)
 }
 
 pub fn get_ec2_node_number() -> u64 {
-    match_line_in_file(&get_ec2_host_port_str(), EC2_HOST_FILE).unwrap_or_else(
-        |err| {
-            panic!(
-                "failed to calculate ec2 node number from host file {}: {}",
-                EC2_HOST_FILE, err
-            )
-        },
-    ) as u64
+    match_line_in_file(&get_ec2_ipv4(), EC2_IP_FILE).unwrap_or_else(|err| {
+        panic!(
+            "failed to calculate ec2 node number from ip file {}: {}",
+            EC2_IP_FILE, err
+        )
+    }) as u64
+}
+
+// Create/overwrite the file with EC2 host information, then return the new file.
+pub fn prepare_ec2_host_file(port: u64) -> &'static str {
+    replace_lines_in_file(EC2_IP_FILE, EC2_HOST_TEMP_FILE, |ipv4| {
+        get_ec2_host_port_str(ipv4, port)
+    })
+    .unwrap();
+    EC2_HOST_TEMP_FILE
 }
