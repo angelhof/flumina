b2d2bb0f5bc86057d69c26c5a38ab8034139ccbf
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-10-24 12:39:07 -0400

    update hosts and script to copy

--- experiments/scripts/timely_update.sh
+++ experiments/scripts/timely_update.sh
@@ -7,9 +7,9 @@ timely_dir=/home/caleb/git/research/flumina-dev/experiments/naiad-experiment
 remote_timely_dir=/home/ubuntu/flumina-devel/experiments/naiad-experiment
 
 echo "Updating Timely files in: $hostname..."
-for dir in src hosts examples
+for dir in src hosts examples Cargo.toml rustfmt.toml
 do
-    echo ">> copying folder: $dir" &&
+    echo ">> copying file or folder: $dir" &&
         ssh $hostname rm -rf ${remote_timely_dir}/${dir} &&
         scp -i ~/.ssh/id_rsa -r -q ${timely_dir}/${dir} $hostname:${remote_timely_dir} &&
         echo "$hostname,$dir" >> /tmp/updated_hosts &&
