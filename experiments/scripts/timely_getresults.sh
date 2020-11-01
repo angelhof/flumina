8dbf0e7698de969d2f0a9fb48c8d5bbac41b9633
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-11-01 10:50:29 -0500

    script to copy experimental results back to local

---
+++ experiments/scripts/timely_getresults.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copy Timely EC2 results files (in results/) back to local
+
+hostname=$1
+
+timely_dir=/home/caleb/git/research/flumina-dev/experiments/naiad-experiment
+remote_timely_dir=/home/ubuntu/flumina-devel/experiments/naiad-experiment
+
+echo "Getting Timely results files in: $hostname..."
+echo ">> copying folder: results/ to: ec2_results/"
+    scp -i ~/.ssh/id_rsa -r -q $hostname:${remote_timely_dir}/results ${timely_dir}/ec2_results &&
+    echo ">> success"
